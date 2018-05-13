67a6f8c534ef91a2d1012e79bcdd4a345fc25a5f
Scott Mackey <Scott.Mackey@clarks.com>
2018-05-13 23:00:02 +0100

    All new files added with basic framework

---
+++ config/webpack.dev.js
@@ -0,0 +1,56 @@
+const path = require("path")
+
+module.exports = {
+  entry: {
+    main: "./src/main.js"
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist/"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          { 
+            loader: "file-loader",
+            options: {
+              name: "[name].html"
+            }
+          },
+          {
+            loader: "extract-loader"
+          },
+          {
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
